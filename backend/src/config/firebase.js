d48c9ea748bc6b5fb20bde78a6577b3e55cc0d52
Aman Kumar Gupta <pg65734@gmail.com>
2025-03-06 10:44:35 +0530

    fixed

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,9 +1,19 @@
 // firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./firestoreKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Ensure the newlines in the key are properly formatted
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  }),
 });
 
 const db = admin.firestore();
